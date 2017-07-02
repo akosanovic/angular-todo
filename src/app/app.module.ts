af23488ff2407d026cb02419e4e3f7e9f9bbd8b6
milanmilosavljevic <milan@vigormediadesign.com>
2017-07-02 22:29:12 +0200

    Creating Main Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { MainCardComponent } from './main-card/main-card.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     FormsModule
   ],
   providers: [],
