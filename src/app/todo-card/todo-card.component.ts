a4bd774b6b24ed40bb760f9437d35c9e46f72ba4
milanmilosavljevic <milan@vigormediadesign.com>
2017-07-03 11:01:51 +0200

    fixing card heading

--- src/app/todo-card/todo-card.component.ts
+++ src/app/todo-card/todo-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoCardComponent implements OnInit {
 
+  cardTitle = "Edit Category Name"
   constructor() { }
 
   ngOnInit() {
